08e825e47a2f81a0a2fc3c59f5f5c1c6990e91c9
Tai874vn <135409751+Tai874vn@users.noreply.github.com>
2025-12-13 23:31:39 +0700

    fix

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,15 +1,22 @@
-import pkg from '@prisma/client';
+import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
-import pgPkg from 'pg';
+import pgPkg from "pg";
 const { Pool } = pgPkg;
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DIRECT_DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
 const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
   adapter,
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
 });
 
 export default prisma;
